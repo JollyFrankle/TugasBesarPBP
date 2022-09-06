d719df021c9b4e817f83594fba3eeb30848018af
Tenzo <ricardolr088@gmail.com>
2022-09-06 19:16:39 +0700

    file tampilan bottom navigation button

--- app/src/main/java/com/example/tugasbesarpbp/HomeActivity.kt
+++ app/src/main/java/com/example/tugasbesarpbp/HomeActivity.kt
@@ -1,20 +1,62 @@
 package com.example.tugasbesarpbp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
 
-        // set title bar
-        val actionbar = supportActionBar
-        //set actionbar title
-        actionbar!!.title = "Home - JogjaKost";
+        val fragment = ListItemFragment()
+        changeFragment(fragment)
+    }
 
-        // hide the action bar
-        actionbar.hide()
+    // change fragment
+    fun changeFragment(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+    }
 
+    // set title bar
+    fun setActionBarTitle(title: String) {
+        supportActionBar?.title = title
+    }
+
+    // options menu
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_right_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    // on options item selected
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.btnLogOut -> {
+                // confirm
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("Log Out")
+                builder.setMessage("Are you sure?")
+                builder.setPositiveButton("Yes") { dialog, which ->
+                    // log out
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+                builder.setNegativeButton("No") { dialog, which ->
+                    // do nothing
+                }
+                val dialog: AlertDialog = builder.create()
+                dialog.show()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/example/tugasbesarpbp/LoginFragment.kt
+++ app/src/main/java/com/example/tugasbesarpbp/LoginFragment.kt
@@ -1,6 +1,7 @@
 package com.example.tugasbesarpbp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -21,6 +22,9 @@ class LoginFragment : Fragment() {
     private lateinit var tilUsername: TextInputLayout
     private lateinit var tilPassword: TextInputLayout
 
+    private var username: String = ""
+    private var password: String = ""
+
     // array of accepted username and password
     private val accepted = arrayOf(
         "admin" to "0932",
@@ -33,9 +37,15 @@ class LoginFragment : Fragment() {
     }
 
     // tidak ada guna?
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // get username and password from arguments
+        arguments?.let {
+            username = it.getString("username").toString()
+            password = it.getString("password").toString()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -54,17 +64,30 @@ class LoginFragment : Fragment() {
         tilUsername = view.findViewById(R.id.tilLoginUsername)
         tilPassword = view.findViewById(R.id.tilLoginPassword)
 
+        // if username & password is not null, show it
+        if (username.isNotEmpty() && password.isNotEmpty()) {
+            tilUsername.editText?.setText(username)
+            tilPassword.editText?.setText(password)
+
+            // add username and password to accepted
+            accepted.plus(username to password)
+
+            // print accepted
+            for(i in accepted) {
+                Log.d("accepted", i.toString())
+            }
+        }
+
         btnLogin.setOnClickListener {
             val username = tilUsername.editText?.text.toString()
             val password = tilPassword.editText?.text.toString()
 
             if (isValid(username, password)) {
                 // login success
-                Snackbar.make(view, "Login success", Snackbar.LENGTH_SHORT).show()
+//                Snackbar.make(view, "Login success", Snackbar.LENGTH_SHORT).show()
 
                 // go to home activity
                 (activity as MainActivity).goToHome()
-
             } else {
                 // login failed
                 Snackbar.make(view, "Login failed", Snackbar.LENGTH_SHORT).show()
