86c66ad954761e3868341626ccf299a572fa9f8d
JollyFrankle <jollyfrankle3@gmail.com>
2022-09-20 12:42:58 +0700

    ~ Navigation Bar, + Frist Time Opened [Splash] Activity

--- app/src/main/java/com/example/tugasbesarpbp/MainActivity.kt
+++ app/src/main/java/com/example/tugasbesarpbp/MainActivity.kt
@@ -4,13 +4,25 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.Fragment
 import com.example.tugasbesarpbp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen().apply {
+//            setKeepOnScreenCondition {
+//                viewModel.isLoading.value
+//            }
+        }
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
