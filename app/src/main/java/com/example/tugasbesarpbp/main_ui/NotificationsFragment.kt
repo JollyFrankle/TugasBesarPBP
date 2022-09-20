e5115565dd64f59a16687a0566f5211d98a9a989
JollyFrankle <jollyfrankle3@gmail.com>
2022-09-20 08:33:27 +0700

    Update 5.01

--- app/src/main/java/com/example/tugasbesarpbp/main_ui/NotificationsFragment.kt
+++ app/src/main/java/com/example/tugasbesarpbp/main_ui/NotificationsFragment.kt
@@ -1,10 +1,11 @@
-package com.example.tugasbesarpbp
+package com.example.tugasbesarpbp.main_ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.tugasbesarpbp.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
+ * Use the [NotificationsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ProfileFragment : Fragment() {
+class NotificationsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,14 +35,14 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        return inflater.inflate(R.layout.fragment_notifications, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         // set actionbar title
-        (activity as HomeActivity).setActionBarTitle("Profile Fragment")
+        (activity as HomeActivity).setActionBarTitle("Notification Fragment")
     }
 
     companion object {
@@ -51,12 +52,12 @@ class ProfileFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
+         * @return A new instance of fragment NotificationsFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
+            NotificationsFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
