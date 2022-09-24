c9fdf5ce71db54c9a3dadde5486fc44264a3b0dc
JollyFrankle <jollyfrankle3@gmail.com>
2022-09-24 08:40:42 +0700

    Bug replacement rail service

--- app/src/main/java/com/example/tugasbesarpbp/RegisterFragment.kt
+++ app/src/main/java/com/example/tugasbesarpbp/RegisterFragment.kt
@@ -18,6 +18,7 @@ import com.google.android.material.textfield.TextInputLayout
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.*
 
 /**
@@ -85,86 +86,96 @@ class RegisterFragment : Fragment() {
             var error = false
 
             CoroutineScope(Dispatchers.IO).launch {
-                if (password.length < 8) {
-                    // check if password is less than 8 characters
-                    _binding?.tilRegisPassword?.error = "Password must be at least 8 characters"
-                    error = true
-                } else if (password != passwordConfirm) {
-                    // check if password and confirm password is same
-                    _binding?.tilRegisPassword?.error = "Password tidak sama"
-                    _binding?.tilRegisPasswordConfirm?.error = "Password tidak sama"
-                    error = true
-                } else {
-                    _binding?.tilRegisPassword?.error = null
-                    _binding?.tilRegisPasswordConfirm?.error = null
-                }
+                withContext(Dispatchers.Main) {
+                    if (password.length < 8) {
+                        // check if password is less than 8 characters
+                        _binding?.tilRegisPassword?.error = "Password must be at least 8 characters"
+                        error = true
+                    } else if (password != passwordConfirm) {
+                        // check if password and confirm password is same
+                        _binding?.tilRegisPassword?.error = "Password tidak sama"
+                        _binding?.tilRegisPasswordConfirm?.error = "Password tidak sama"
+                        error = true
+                    } else {
+                        _binding?.tilRegisPassword?.error = null
+                        _binding?.tilRegisPasswordConfirm?.error = null
+                    }
 
-                // check if email format is valid using regex
-                if (!email.matches(Regex("^[A-Za-z](.*)([@]{1})(.{1,})(\\.)(.{1,})"))) {
-                    _binding?.tilRegisEmail?.error = "Email tidak valid"
-                    error = true
-                } else {
-                    _binding?.tilRegisEmail?.error = null
-                }
+                    // check if email format is valid using regex
+                    if (!email.matches(Regex("^[A-Za-z](.*)([@]{1})(.{1,})(\\.)(.{1,})"))) {
+                        _binding?.tilRegisEmail?.error = "Email tidak valid"
+                        error = true
+                    } else {
+                        _binding?.tilRegisEmail?.error = null
+                    }
 
-                // check if name is empty
-                if (username.isEmpty()) {
-                    _binding?.tilRegisUsername?.error = "Username tidak boleh kosong"
-                    error = true
-                } else {
-                    _binding?.tilRegisUsername?.error = null
-                }
+                    // check if name is empty
+                    if (username.isEmpty()) {
+                        _binding?.tilRegisUsername?.error = "Username tidak boleh kosong"
+                        error = true
+                    } else {
+                        _binding?.tilRegisUsername?.error = null
+                    }
 
-                // check if username is empty
-                if (name.isEmpty()) {
-                    binding.tilRegisNama.error = "Username tidak boleh kosong"
-                    error = true
-                } else {
-                    binding.tilRegisNama.error = null
-                }
+                    // check if username is empty
+                    if (name.isEmpty()) {
+                        binding.tilRegisNama.error = "Nama tidak boleh kosong"
+                        error = true
+                    } else {
+                        binding.tilRegisNama.error = null
+                    }
 
-                if(tanggalLahir.isEmpty()){
-                    _binding?.tilRegisTanggalLahir?.error = "Tanggal lahir harus diisi"
-                    error = true
-                } else{
-                    _binding?.tilRegisTanggalLahir?.error = null
-                }
+                    if(tanggalLahir.isEmpty()){
+                        _binding?.tilRegisTanggalLahir?.error = "Tanggal lahir harus diisi"
+                        error = true
+                    } else{
+                        _binding?.tilRegisTanggalLahir?.error = null
+                    }
 
-                if(nomorTelepon.length == 12){
-                    _binding?.tilRegisNomorTelepon?.error = null
-                } else{
-                    _binding?.tilRegisNomorTelepon?.error = "Nomor telepon harus 12 digit"
-                    error = true
-                }
+                    if(nomorTelepon.length == 12){
+                        _binding?.tilRegisNomorTelepon?.error = null
+                    } else{
+                        _binding?.tilRegisNomorTelepon?.error = "Nomor telepon harus 12 digit"
+                        error = true
+                    }
 
-                // check if username is already taken
-                if(userDao.getUserByUsernameOrEmail(username, email) != null) {
-                    _binding?.tilRegisUsername?.error = "Username atau email sudah terdaftar"
-                    _binding?.tilRegisEmail?.error = "Username atau email sudah terdaftar"
-                    error = true
-                } else {
-                    _binding?.tilRegisUsername?.error = null
-                    _binding?.tilRegisEmail?.error = null
-                }
+                    if (!error) {
+                        // check if username is already taken
+                        if (userDao.getUserByUsernameOrEmail(username, email) != null) {
+                            _binding?.tilRegisUsername?.error =
+                                "Username atau email sudah terdaftar"
+                            _binding?.tilRegisEmail?.error = "Username atau email sudah terdaftar"
+                            error = true
+                        } else {
+                            _binding?.tilRegisUsername?.error = null
+                            _binding?.tilRegisEmail?.error = null
+                        }
+                    }
 
-                // all input is valid
-                if (!error) {
-                    val user = User(0, name, username, password, email, tanggalLahir, nomorTelepon)
-                    userDao.addUser(user)
+                    // all input is valid
+                    if(!error) {
+                        val user =
+                            User(0, name, username, password, email, tanggalLahir, nomorTelepon)
+                        userDao.addUser(user)
 
-                    Snackbar.make(view, "Register berhasil!", Snackbar.LENGTH_SHORT).show()
+                        Snackbar.make(view, "Register berhasil!", Snackbar.LENGTH_SHORT).show()
 
-                    val loginFragment: Fragment = LoginFragment()
-                    // add username and password to bundle
-                    val bundle = Bundle()
-                    bundle.putString("username", username)
-                    bundle.putString("password", password)
-                    loginFragment.arguments = bundle
+                        val loginFragment: Fragment = LoginFragment()
+                        // add username and password to bundle
+                        val bundle = Bundle()
+                        bundle.putString("username", username)
+                        bundle.putString("password", password)
+                        loginFragment.arguments = bundle
 
-                    // change fragment to login fragment
-                    (activity as MainActivity).changeFragment(loginFragment)
-                } else {
-                    Snackbar.make(view, "Register gagal. Cek ulang bagian yang ditandai.", Snackbar.LENGTH_LONG).show()
+                        // change fragment to login fragment
+                        (activity as MainActivity).changeFragment(loginFragment)
+                    } else {
+                        Snackbar.make(
+                            view,
+                            "Register gagal. Cek ulang bagian yang ditandai.",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                    }
                 }
             }
         }
