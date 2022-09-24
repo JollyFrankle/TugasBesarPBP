aef94d6298c640b77aa446e67e4292474a9c2703
JollyFrankle <jollyfrankle3@gmail.com>
2022-09-24 12:37:55 +0700

    Komit

--- app/src/main/java/com/example/tugasbesarpbp/MainActivity.kt
+++ app/src/main/java/com/example/tugasbesarpbp/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.tugasbesarpbp
 
 import android.app.Activity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.fragment.app.Fragment
 import com.example.tugasbesarpbp.databinding.ActivityMainBinding
 
@@ -15,6 +23,9 @@ class MainActivity : AppCompatActivity() {
 
     private val viewModel: MainViewModel by viewModels()
     private lateinit var spSession: SharedPreferences
+
+    private val CHANNEL_ID = "channel_notification_01"
+    private val notificationId = 101
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,9 +39,20 @@ class MainActivity : AppCompatActivity() {
         // set statusBarColor
         window.statusBarColor = resources.getColor(R.color.color_secondary_variant, null)
 
-        // go to login fragment
-        val fragment = LoginFragment()
-        changeFragment(fragment)
+        createNotificationChannel()
+
+        // get fragment from intent
+        val action = intent.getStringExtra("action")
+        println("------------------------------ " + action + intent.getBundleExtra("data").toString())
+        if(action == "login") {
+            val fragment = LoginFragment()
+            fragment.arguments = intent.getBundleExtra("data")
+            changeFragment(fragment)
+        } else {
+            // go to login fragment
+            val fragment = LoginFragment()
+            changeFragment(fragment)
+        }
     }
 
     fun changeFragment(fragment: Fragment) {
@@ -50,5 +72,62 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, HomeActivity::class.java)
         startActivity(intent)
         finish()
+    }
+
+    private fun createNotificationChannel() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Notification Title"
+            val descriptionText = "Notification Description"
+
+            val channel1 = NotificationChannel(
+                CHANNEL_ID,
+                name,
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply {
+                description = descriptionText
+            }
+
+            val notificationManager: NotificationManager =
+                getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel1)
+        }
+    }
+
+    fun sendRegisterNotification(name: String, username: String, password: String) {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+        // Siapkan intent untuk membuka aplikasi ke bagian login, auto isi username dan password
+        val broadcastIntent = Intent(this, NotificationReceiver::class.java)
+        val bundle = Bundle()
+        bundle.putString("username", username)
+        bundle.putString("password", password)
+        broadcastIntent.putExtra("action", "main_activity")
+        broadcastIntent.putExtra("data", bundle)
+        val actionIntent = PendingIntent.getBroadcast(this, 0, broadcastIntent, PendingIntent.FLAG_CANCEL_CURRENT)
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_baseline_notifications_24)
+            .setContentTitle("Halo, $name! 👋👋")
+            .setContentText("Selamat bergabung di JogjaKost! Silakan login.")
+            .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+            .setAutoCancel(true)
+            .setOnlyAlertOnce(true)
+            .setContentIntent(pendingIntent)
+            .setColor(Color.BLUE)
+            .addAction(R.mipmap.ic_launcher, "Log in", actionIntent)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        val pictureBitmap = BitmapFactory.decodeResource(resources, R.drawable.ic_launcher_background)
+        // Big picture style
+        val bigPictureStyle = NotificationCompat.BigPictureStyle(builder)
+            .bigPicture(pictureBitmap)
+
+        with(NotificationManagerCompat.from(this)) {
+            notify(notificationId, bigPictureStyle.build()!!)
+        }
     }
 }
--- app/src/main/java/com/example/tugasbesarpbp/NotificationReceiver.kt
+++ app/src/main/java/com/example/tugasbesarpbp/NotificationReceiver.kt
@@ -4,10 +4,21 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 
 class NotificationReceiver: BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent){
-        val message = intent.getStringExtra("toastMessage")
-        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+        // get the action from the intent
+        val action = intent.getStringExtra("action")
+        when(action) {
+            "main_activity" -> {
+                // start main activity
+                val mainActivityIntent = Intent(context, MainActivity::class.java)
+                mainActivityIntent.putExtra("action", "login")
+                mainActivityIntent.putExtra("data", intent.getBundleExtra("data"))
+                mainActivityIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(context!!, mainActivityIntent, null)
+            }
+        }
     }
 }
