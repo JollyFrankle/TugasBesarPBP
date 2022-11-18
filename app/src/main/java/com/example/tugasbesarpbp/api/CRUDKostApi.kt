e458edf3ddc0b54bd41acac68f012e23d23f8bf2
CalvinAndrean <calvinandrean456@gmail.com>
2022-11-18 13:28:01 +0700

    CRUDKostApi
    Menghindari overwritten

---
+++ app/src/main/java/com/example/tugasbesarpbp/api/CRUDKostApi.kt
@@ -0,0 +1,11 @@
+package com.example.tugasbesarpbp.api
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CRUDKostApi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crudkost_api)
+    }
+}
---
+++ app/src/main/java/com/example/tugasbesarpbp/api/KostApi.kt
@@ -0,0 +1,13 @@
+package com.example.tugasbesarpbp.api
+
+class KostApi {
+    companion object{
+        val BASE_URL = "http://20.168.252.140/UGD_PBP/public/api/"
+
+        val GET_ALL_URL = BASE_URL + "kost/"
+        val GET_BY_ID_URL = BASE_URL + "kost/"
+        val ADD_URL = BASE_URL + "kost/"
+        val UPDATE_URL = BASE_URL + "kost/"
+        val DELETE_URL = BASE_URL + "kost/"
+    }
+}
